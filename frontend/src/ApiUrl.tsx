17e017fa4ca436c130ce8c14e2e12b58b7c32e77
Abbalolo <verifiedaboki@gmail.com>
2024-04-10 18:01:08 +0100

    error

--- frontend/src/ApiUrl.tsx
+++ frontend/src/ApiUrl.tsx
@@ -1,2 +1,4 @@
-export const url = "https://codsoft-1-w194.onrender.com";
-export const imgF = "https://codsoft-1-w194.onrender.com/images/";
+// export const url = "https://codsoft-1-w194.onrender.com";
+// export const imgF = "https://codsoft-1-w194.onrender.com/images/";
+export const url = "http://localhost:5000";
+export const imgF = "http://localhost:5000/images/";
--- frontend/src/Pages/post/PostDetails.tsx
+++ frontend/src/Pages/post/PostDetails.tsx
@@ -22,6 +22,7 @@ export interface Post {
   categories: string[]
   userId: string;
 }
+
 export interface MyComment {
   id: any;
   author: string;
@@ -133,77 +134,115 @@ const fetchComments = async () => {
 
 
 return (
-  <main className="px-6 md:px-[150px] py-10 ">
-    <div className="flex flex-col">
-      <div className="flex justify-between ">
-        <h1 className="text-xl font-bold md:mb-2 md:text-2xl">{post.title}</h1>
-        <div className="flex items-center gap-4 text-xl">
-          {user?._id === post.userId && (
-            <>
-              <button onClick={() => navigate(`/edit/${postId}`)} >
-               <FiEdit />
-              </button>
-              <button onClick={deletePost}>
-                <TbTrashFilled />
-              </button>
-            </>
-          )}
+  <main className="px-6 md:px-[100px] py-10 flex flex-col md:flex-row  w-full justify-between gap-5">
+    <div className="md:w-[40%] ">
+      <div className="flex flex-col md:hidden">
+        <div className="flex justify-between ">
+          <h1 className="text-xl font-bold md:mb-2 md:text-2xl">
+            {post.title}
+          </h1>
+          <div className="flex items-center gap-4 text-xl">
+            {user?._id === post.userId && (
+              <>
+                <button onClick={() => navigate(`/edit/${postId}`)}>
+                  <FiEdit />
+                </button>
+                <button onClick={deletePost}>
+                  <TbTrashFilled />
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+        <div className="flex items-center mt-5 justify-between mb-2 text-sm font-semibold text-gray-500 space-x-4 md:mb-4">
+          <p>@{post.username}</p>
+          <div className="flex space-x-2">
+            <p>{new Date(post.updatedAt).toString().slice(0, 15)}</p>
+            <p>{new Date(post.updatedAt).toString().slice(15, 24)}</p>
+          </div>
         </div>
       </div>
-      <div className="flex items-center mt-5 justify-between mb-2 text-sm font-semibold text-gray-500 space-x-4 md:mb-4">
-        <p>@{post.username}</p>
-        <div className="flex space-x-2">
-          <p>{new Date(post.updatedAt).toString().slice(0, 15)}</p>
-          <p>{new Date(post.updatedAt).toString().slice(15, 24)}</p>
-        </div>
-      </div>
-    </div>
-
-    <img
-      src={imgF + post.photo}
-      alt="Post"
-      className="h-full w-full object-cover"
-    />
-    <p className="text-sm md:text-lg py-3">{post.description}</p>
-
-    <div className="flex items-center mt-3 space-x-4 font-semibold">
-      <p>Categories:</p>
-      <div className="flex items-center  space-x-2 text-sm ">
-        {post.categories.length !== 0
-          ? post.categories.map((cat, index) => (
-              <div key={index} className="bg-gray-300 rounded-md px-3 py-1">
-                {cat}
-              </div>
-            ))
-          : null}
-      </div>
-    </div>
-    <div className="flex flex-col mt-4">
-      <h3 className="flex items-center  font-semibold">Comments:</h3>
-      <div className="flex flex-col gap-3">
-        {Array.isArray(comments) &&
-          comments.map((c) => (
-            <div key={c.id}>
-              <Comment comment={c} post={post}   fetchComments={ fetchComments} />
-            </div>
-          ))}
-      </div>
-
-      <form
-        onSubmit={addComment}
-        className="w-full flex flex-col mt-4 md:flex-row md:gap-4 "
-      >
-        <input
-          type="text"
-          placeholder="Write a comment"
-          className="md:w-[80%] outline-none px-4 py-2 mt-4 md:mt-0 border rounded-md"
-          value={commentText}
-          onChange={(e) => setCommentText(e.target.value)}
+      <div className="">
+        <img
+          src={imgF + post.photo}
+          alt="Post"
+          className="h-full w-full object-cover"
         />
-        <button className="bg-black text-white px-4 py-2 md:w-[30%] mt-4 md:mt-0 text-sm md:text-base rounded-md">
-          Add comment
-        </button>
-      </form>
+      </div>
+    </div>
+
+    <div className="md:w-[60%] ">
+      <div className=" flex-col hidden md:flex">
+        <div className="flex justify-between ">
+          <h1 className="text-xl font-bold md:mb-2 md:text-2xl">
+            {post.title}
+          </h1>
+          <div className="flex items-center gap-4 text-xl">
+            {user?._id === post.userId && (
+              <>
+                <button onClick={() => navigate(`/edit/${postId}`)}>
+                  <FiEdit />
+                </button>
+                <button onClick={deletePost}>
+                  <TbTrashFilled />
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+        <div className="flex items-center mt-5 justify-between mb-2 text-sm font-semibold text-gray-500 space-x-4 md:mb-4">
+          <p>@{post.username}</p>
+          <div className="flex space-x-2">
+            <p>{new Date(post.updatedAt).toString().slice(0, 15)}</p>
+            <p>{new Date(post.updatedAt).toString().slice(15, 24)}</p>
+          </div>
+        </div>
+      </div>
+      <p className="text-sm md:text-lg py-3">{post.description}</p>
+
+      <div className="flex items-center mt-3 space-x-4 font-semibold">
+        <p>Categories:</p>
+        <div className="flex items-center  space-x-2 text-sm ">
+          {post.categories.length !== 0
+            ? post.categories.map((cat, index) => (
+                <div key={index} className="bg-gray-300 rounded-md px-3 py-1">
+                  {cat}
+                </div>
+              ))
+            : null}
+        </div>
+      </div>
+      <div className="flex flex-col mt-4">
+        <h3 className="flex items-center  font-semibold">Comments:</h3>
+        <div className="flex flex-col gap-3">
+          {Array.isArray(comments) &&
+            comments.map((c) => (
+              <div key={c.id}>
+                <Comment
+                  comment={c}
+                  post={post}
+                  fetchComments={fetchComments}
+                />
+              </div>
+            ))}
+        </div>
+
+        <form
+          onSubmit={addComment}
+          className="w-full flex flex-col mt-4 md:flex-row md:gap-4 "
+        >
+          <input
+            type="text"
+            placeholder="Write a comment"
+            className="md:w-[80%] outline-none px-4 py-2 mt-4 md:mt-0 border rounded-md"
+            value={commentText}
+            onChange={(e) => setCommentText(e.target.value)}
+          />
+          <button className="bg-black text-white px-4 py-2 md:w-[30%] mt-4 md:mt-0 text-sm md:text-base rounded-md">
+            Add comment
+          </button>
+        </form>
+      </div>
     </div>
   </main>
 );
